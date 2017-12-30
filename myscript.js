4a97db9ebcd14e0a86e28ab499b4901c290f7e9e
Marcin Jablonowski <marcin.jab@wp.pl>
2017-12-30 23:31:26 +0100

    js

--- myscript.js
+++ myscript.js
@@ -1,8 +1,25 @@
 alert("Hello")
-var deposit = prompt('How much deposit:');
-alert('Deposit: '+ deposit);
-console.log('You are cool porson')
+//var deposit = prompt('How much deposit:');
+//alert('Deposit: '+ deposit);
+//console.log('You are cool porson')
 
+// control flow
+var hot = false;
+var temp = prompt('What is temp?');
+
+if (temp > 80) {
+    console.log('Hot Outside!');
+} else if (temp <= 80 && temp >= 50) {
+    console.log('Average temp Outside');
+} else if (temp < 50 && temp >= 32){
+    console.log('Its pretty cold out!');
+} else {
+    console.log('Its very cold out');
+}
+
+console.log (hot)
+
+// while loop
 var x=0;
 
 while (x < 5) {
@@ -17,10 +34,18 @@ while (x < 5) {
 }
 console.log('End while loop')
 
-var num=1;
+var num = 1;
 while(num < 11) {
-    if (num%2 === 0) {
+    if (num % 2 === 0) {
         console.log(num);
     }
     num = num + 1;
+}
+// for loop
+for (var i = 0; i < 5; i++) {
+    console.log('Number is '+ (i+1));
+}
+var word = 'ABCDEFGHIJK';
+for (var i = 0; i < word.length; i++) {
+    console.log(word[i]);
 }
